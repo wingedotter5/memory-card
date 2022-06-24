8b72c7f87e3adc4d7d5363e596c2742f3e1b0bb7
Vishal Kumar <visalkoumar@gmail.com>
2022-06-24 12:52:08 +0530

    Add hint

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,10 @@ import classes from "../styles/Header.module.css";
 export default function Header({ score, highScore }) {
   return (
     <header className={classes.header}>
-      <h1>Memory Game</h1>
+      <div>
+        <h1>Rick &amp; Morty Memory Game</h1>
+        <h3>( Click all of them, but not twice! )</h3>
+      </div>
       <div className={classes.score}>
         <h2>{`Score: ${score * 100}`}</h2>
         <h2>{`Best Score: ${highScore * 100}`}</h2>
